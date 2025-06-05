e6bc51b05226507232e9efa6b4d3e369d3817486
Anish <sharmaanish256@gmail.com>
2025-06-05 23:37:22 +0530

    setup basic ocr

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import Tesseract from "tesseract.js";
 
 const TOKEN = process.env.BOT_TOKEN;
 if (!TOKEN) throw new Error("Token not found!");
 const bot = new Telegraf(TOKEN);
+bot.on(message("photo"), async (ctx) => {
+  const fileId = ctx.message.photo[2].file_id;
+  const fileLinks = await bot.telegram.getFileLink(fileId);
+  const { href } = fileLinks;
+  const data = await Tesseract.recognize(href);
+  if (data.data.text) {
+    ctx.sendMessage(data.data.text);
+  } else ctx.sendMessage("Failed to recognise text!");
+});
 bot.start((ctx) => ctx.reply("Welcome! Start Tracking Expenses."));
 bot.launch(() => console.log("Bot Listening!"));
