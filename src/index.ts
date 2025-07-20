7ef92c298834496a214c3fd57c58d5f0e24e4bb8
Anish Sharma <sharmaanish256@gmail.com>
2025-07-20 18:48:09 +0530

    comment fix

--- src/index.ts
+++ src/index.ts
@@ -186,21 +186,21 @@ async function askNextTransaction(ctx: MyContext) {
 
 bot.command("get_categories", async (ctx) => {
   const categories = await getCategories();
-  // if (
-  //   categories &&
-  //   categories.length > 0 &&
-  //   categories.every((each) => typeof each === "string")
-  // ) {
-  //   ctx.session.categories = categories;
-  //   await ctx.reply(
-  //     `✅ Categories loaded from sheet:\n\n${categories.join(", ")}`
-  //   );
-  // } else {
-  await ctx.reply(
-    "❌ Failed to detect categories from sheet. Please set them manually by sending:\n\n`/set_categories Food, Travel, Shopping`\n\n(Comma-separated list)",
-    { parse_mode: "Markdown" }
-  );
-  // }
+  if (
+    categories &&
+    categories.length > 0 &&
+    categories.every((each) => typeof each === "string")
+  ) {
+    ctx.session.categories = categories;
+    await ctx.reply(
+      `✅ Categories loaded from sheet:\n\n${categories.join(", ")}`
+    );
+  } else {
+    await ctx.reply(
+      "❌ Failed to detect categories from sheet. Please set them manually by sending:\n\n`/set_categories Food, Travel, Shopping`\n\n(Comma-separated list)",
+      { parse_mode: "Markdown" }
+    );
+  }
 });
 
 bot.command("set_categories", async (ctx) => {
